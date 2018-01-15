699661a449a5df87ef1a616556eb46c661dfe0b6
Nicolas Marshall <marshall.nicolas@gmail.com>
2018-01-15 15:50:05 +0100

    parse home tilde, use absolute paths

--- copy.go
+++ copy.go
@@ -1,16 +1,61 @@
 package cp
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
 )
+
+func replaceHomeFolder(path string) (string, error) {
+	if !strings.HasPrefix(path, "~") {
+		return path, nil
+	}
+	var buffer bytes.Buffer
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	_, err = buffer.WriteString(usr.HomeDir)
+	if err != nil {
+		return "", err
+	}
+	_, err = buffer.WriteString(strings.TrimPrefix(path, "~"))
+	if err != nil {
+		return "", err
+	}
+
+	return buffer.String(), nil
+}
+
+func AbsolutePath(path string) (string, error) {
+	homeReplaced, err := replaceHomeFolder(path)
+	if err != nil {
+		return "", err
+	}
+	return filepath.Abs(homeReplaced)
+}
 
 // CopyFile copies a file from src to dst. If src and dst files exist, and are
 // the same, then return success. Otherwise, attempt to create a hard link
 // between the two files. If that fails, copy the file contents from src to dst.
 func CopyFile(src, dst string) (err error) {
-	sfi, err := os.Stat(src)
+	srcAbs, err := AbsolutePath(src)
+	if err != nil {
+		return err
+	}
+	dstAbs, err := AbsolutePath(dst)
+	if err != nil {
+		return err
+	}
+
+	// open source file
+	sfi, err := os.Stat(srcAbs)
 	if err != nil {
 		return
 	}
@@ -19,11 +64,20 @@ func CopyFile(src, dst string) (err error) {
 		// symlinks, devices, etc.)
 		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
 	}
-	dfi, err := os.Stat(dst)
+
+	// open dest file
+	dfi, err := os.Stat(dstAbs)
 	if err != nil {
 		if !os.IsNotExist(err) {
 			return
 		}
+		// file doesn't exist
+		spew.Dump(filepath.Dir(dst))
+		err := os.MkdirAll(filepath.Dir(dst), 0755)
+		if err != nil {
+			spew.Dump(err)
+		}
+
 	} else {
 		if !(dfi.Mode().IsRegular()) {
 			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
@@ -35,8 +89,7 @@ func CopyFile(src, dst string) (err error) {
 	if err = os.Link(src, dst); err == nil {
 		return
 	}
-	err = copyFileContents(src, dst)
-	return
+	return copyFileContents(src, dst)
 }
 
 // copyFileContents copies the contents of the file named src to the file named
